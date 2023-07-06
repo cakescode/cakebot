9c1928b3103f255d59e440c1ca4128cc6bebd9b2
cake <3rdbinarybug@gmail.com>
2023-07-05 22:42:45 -0400

    many fixes/changes
    some formatting fixes, removing non-working commands, attempting a looped task (not working yet) as a reminder function

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,29 +1,23 @@
 from discord.ext import tasks, commands
 import datetime, asyncio
-from __main__ import bot
 
 class reminder(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+	
+	loop_val = 1
 
-	# @commands.slash_command()
-	# async def hello(self, ctx):
-	# 	await ctx.respond("Oh, Hi there.")
-
-	# @commands.Cog.listener()
-	# async def on_message(self, message):
-	# 	if message.content.startswith('!testing'):
-	# 		await message.channel.send(f'This is a command with a listener')
-
-	# 	if message.content.startswith('!say'):
-	# 		await message.channel.send(f'{message.content.replace("!say", "")}')
-
-	@bot.event
-	async def coding_reminder(self, ctx):
-		while True:
-			if '17:00:00' <= datetime.datetime.now() <= '17:10:00':
-				await ctx.send('time test')
-				await asyncio.sleep(600)
+	@tasks.loop(minutes=5)
+	async def coding_reminder(self):
+		await print(f'debug: loop lap {loop_val}')
+		loop_val += 1
+		reminder_channel = self.bot.get_channel(1125139363517436014)
+		if '22:35:00' <= datetime.datetime.now() <= '22:45:00':
+			await reminder_channel.send('time test')
+			await print('debug: time test')
+			await asyncio.sleep(600)
 
 def setup(bot):
 	bot.add_cog(reminder(bot))
+
+# TODO : Fix coding_reminder()
--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 """
 TO INSTALL:
 
-pip install discord.py
 pip install py-cord
+pip install translate
+pip install 
+pip install 
 
 """
-
 import os
 import sys
 import discord
 from discord.ext import commands, tasks
 
 # This sets the prefix to use for commands. Here, we use a Slash. 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('>'), intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('>'),
+                   intents=discord.Intents.all())
 
-
-# In this function, we load all the files
-# from the Cogs folder. Cogs are just files
-# that hold our commands.
+# In this function, we load all the files from the Cogs folder.
+# Cogs are just files that hold our commands.
 def load_cogs():
     for file in os.listdir('./cogs'):
             if file.endswith('.py') and not file.startswith('_'):
                 bot.load_extension('cogs.' + file[:-3])
 
-
-# In this function, we use an argument to load
-# the Bot-Token.
-# To run your bot, you would use:
-# python main.py TOKEN
+# In this function, we use an argument to load the Bot-Token.
+# To run your bot, you would use: python main.py TOKEN
 def load_token_and_run():
     if len(sys.argv) > 1:
         TOKEN = sys.argv[1]
@@ -35,13 +32,6 @@ def load_token_and_run():
     else:
         print('ERROR: You must include a bot token.')
 
-
 if __name__ == '__main__':
     load_cogs()
     load_token_and_run()
-
-
-
-
-
-
