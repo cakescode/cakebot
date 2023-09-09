a57ef694f3a7926f93c4e2a89563ce586460ab3f
cake <3rdbinarybug@gmail.com>
2023-09-09 03:19:40 -0400

    optimization
    is_multi_lang was not needed, neither was reversing self.gl as googletrans has a premade backwards dict (LANGCODES)

--- cogs/trans.py
+++ cogs/trans.py
@@ -2,15 +2,11 @@ import discord.ext, googletrans, random
 from discord.ext import commands
 from __main__ import traceback
 
-def is_multi_lang(lang):
-	if isinstance(lang, list): return True
-	else: return False
-
 class googletrans_func(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.gl = googletrans.LANGUAGES # all supported langs in a dict
-		self.gl_rvs = {lang: code for code, lang in self.gl.items()} # reverse of self.gl
+		self.gl_codes = googletrans.LANGCODES # reverse of self.gl
 		self.translator = googletrans.Translator()
 		self.THRESHOLD = 0.85 # if confidence lower than this & not multi_lang then return
 		self.LANGUAGE = 'en' # print(googletrans.LANGUAGES) to get lang codes
@@ -18,12 +14,13 @@ class googletrans_func(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
+		# initial check
 		if message.author.bot: return
 
 		try: 
 			detected = self.translator.detect(message.content)
 			lang = detected.lang
-			multi_lang = is_multi_lang(lang)
+			multi_lang = isinstance(lang, list)
 
 			# if LANGUAGE or within the confidence THRESHOLD of LANGUAGE then return
 			if lang == self.LANGUAGE or multi_lang and self.LANGUAGE in lang: return
@@ -31,13 +28,11 @@ class googletrans_func(commands.Cog):
 			
 			# if detection picks up 2 potential languages
 			if multi_lang:
-				detected.confidence = detected.confidence[0] # extract value from list
-				lang, lang_2 = lang[0], lang[1]
-				detected_language = f'{self.gl[lang]}, {self.gl[lang_2]}' # for output
-
 				# quick fix bc the self.gl keys dont match with API dict from detect()
 				for l in range(2):
 					if lang[l] == 'zh-CH': lang[l] = 'zh-ch'
+				detected.confidence = detected.confidence[0] # extract value from list
+				detected_language = f'{self.gl[lang[0]]}/{self.gl[lang[1]]}' # for output
 
 			else:
 				if lang == 'zh-CN': lang = 'zh-cn'
@@ -45,11 +40,15 @@ class googletrans_func(commands.Cog):
 
 			# translate message to english
 			translation = self.translator.translate(message.content, dest=self.LANGUAGE)
+			print(translation) # debug
+			if translation.pronunciation == None: pronunciation = ''
+			else: pronunciation = f'pronunciation: {translation.pronunciation}\n'
 
 			# send results of translation as embed
 			embed = discord.Embed(title=f'{message.content}:',
 				description=f'{translation.text}')
-			embed.set_footer(text=(f'translated from {detected_language} to english'
+			embed.set_footer(text=(f'{pronunciation}'
+				f'translated from {detected_language} to english'
 				f'\nauto detection confidence: {detected.confidence * 100}%'))
 			await message.channel.send(embed=embed)
 
@@ -59,12 +58,14 @@ class googletrans_func(commands.Cog):
 
 	@commands.slash_command(description='Type /languages to get a list of supported languages.')
 	async def translate(self, ctx, langauge, message):
+		# initial checks
 		if langauge.casefold() in ('chinese', 'chinese simplified'): langauge = 'chinese (simplified)'
 		elif langauge.casefold() in ('chinese traditional'): langauge = 'chinese (traditional)'
 		elif langauge.casefold() in ('kurdish', 'kurmanji'): langauge = 'kurdish (kurmanji)'
 		elif langauge.casefold() in ('myanmar', 'burmese'): langauge = 'myanmar (burmese)'
+
 		try:
-			translated = self.translator.translate(message, dest=self.gl_rvs[langauge.casefold()])
+			translated = self.translator.translate(message, dest=self.gl_codes[langauge.casefold()])
 			embed = discord.Embed(title=f'{message}:', description=translated.text)
 			embed.set_footer(text=f'translated from english to {langauge}')
 
