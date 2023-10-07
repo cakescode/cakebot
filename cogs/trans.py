26caeaed6b4ce9d15ed6073be8f759872a414c87
cake <3rdbinarybug@gmail.com>
2023-10-07 06:12:09 -0400

    update/trans.py
    some cleaning on the checks and logic flow, as well as some other small changes including a check to prevent useless translation responses. also testing a json blacklist for words not to be translated even if they fit the criteria of all the rest of the checks

--- cogs/trans.py
+++ cogs/trans.py
@@ -1,4 +1,4 @@
-import discord.ext, googletrans, random
+import discord.ext, googletrans, json
 from discord.ext import commands
 from __main__ import traceback
 
@@ -10,45 +10,47 @@ class googletrans_func(commands.Cog):
 		self.translator = googletrans.Translator()
 		self.THRESHOLD = 0.85 # if confidence lower than this & not multi_lang then return
 		self.LANGUAGE = 'en' # print(googletrans.LANGUAGES) to get lang codes
-
+		self.N = '\n\n'
+		self.COLOR = discord.Color.red()
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
-		# initial check
+
+		# initial checks
 		if message.author.bot: return
 
 		try: 
+			# with json.load(open('data.json', 'r'))['blacklist'] as blacklist:
+			# 	if message.content in blacklist: return
 			detected = self.translator.detect(message.content)
 			lang = detected.lang
-			multi_lang = isinstance(lang, list)
-
-			# if LANGUAGE or within the confidence THRESHOLD of LANGUAGE then return
-			if lang in self.LANGUAGE: return
-			if detected.confidence < self.THRESHOLD and not multi_lang: return
+			multi_lang = isinstance(lang, list) # check if output is list
 			
 			# if detection picks up 2 potential languages
 			if multi_lang:
 				# quick fix bc the self.gl keys dont match with API dict from detect()
-				for l in len(lang):
+				for l in lang:
 					l = l.lower()
-				detected.confidence = detected.confidence[0] # extract value from list
+				detected.confidence = detected.confidence[0] * 100 # extract value from list
 				detected_language = f'{self.gl[lang[0]]}/{self.gl[lang[1]]}' # for output
-
+			# if LANGUAGE or within the confidence THRESHOLD of LANGUAGE then return
+			elif lang in self.LANGUAGE: return
+			elif detected.confidence < self.THRESHOLD: return
 			else:
 				lang = lang.lower()
 				detected_language = f'{self.gl[lang]}'
 
 			# translate message to english
 			translation = self.translator.translate(message.content, dest=self.LANGUAGE)
-			pronunciation = translation.extra_data['translation'][1:]
-			n = '\n'
+			if translation == message.content: return # if translation == message, dont translate
+			pronunciation = translation.extra_data['translation'][-1]
 
 			# send results of translation as embed
 			embed = discord.Embed(title=f'{message.content}:',
-				description=(f'{pronunciation[0][-1] + n if pronunciation else ""}'
+				description=(f'{str(pronunciation[-1]).lower() + self.N if pronunciation else ""}'
 					f'"{translation.text}"'))
 			embed.set_footer(text=(f'translated from {detected_language} to english'
-				f'\nauto detection confidence: {detected.confidence * 100}%'))
+				f'\nauto detection confidence: {detected.confidence}%'))
 			await message.channel.send(embed=embed)
 
 		# if error, send error message to channel that caused it
@@ -56,9 +58,9 @@ class googletrans_func(commands.Cog):
 
 
 	@commands.slash_command(description=('Example: `/translate from english to japanese hello world`'))
-	async def translate(self, ctx, _from, _to, message):
+	async def translate(self, ctx, old, new, message):
 		# initial checks
-		checklist = (_from, _to)
+		checklist = (old, new)
 		for item in checklist:
 			if item.casefold() in ('chinese', 'chinese simplified'): item = 'chinese (simplified)'
 			elif item.casefold() in ('chinese traditional'): item = 'chinese (traditional)'
@@ -66,13 +68,12 @@ class googletrans_func(commands.Cog):
 			elif item.casefold() in ('myanmar', 'burmese'): item = 'myanmar (burmese)'
 
 		try:
-			translation = self.translator.translate(message, dest=self.gl_codes[_to.casefold()])
-			pronunciation = translation.extra_data['translation'][1:]
-			n = '\n'
+			translation = self.translator.translate(message, dest=self.gl_codes[new.casefold()])
+			pronunciation = translation.extra_data['translation'][-1]
 			embed = discord.Embed(title=f'{message}:', description=(
-				f'{pronunciation[0][-1] + n if pronunciation else ""}'
+				f'{str(pronunciation[-1]).lower() + self.N if pronunciation else ""}'
 				f'"{translation.text}"'))
-			embed.set_footer(text=f'translated from {_from} to {_to}')
+			embed.set_footer(text=f'translated from {old} to {new}')
 
 		except ValueError:
 			embed = discord.Embed(title='Not a supported language!',
